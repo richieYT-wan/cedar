5461085ebc7eb4bd0148b6bc589f2fc862787f6e
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2022-05-20 16:08:29 +0200

    updated pipeline

---
+++ scripts/kmers_pipeline.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+# Activates conda environment
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate phd
+
+# $1 should be the input directory or filepath, relative to DIR
+# ex, if dir is ./cedar/data/human_split/, then $1 should be "data/human_split/"
+
+# $2 should be the target filename: i.e. "human" if running for human
+# TODO: $3 should be the split number when implementing with splitting from a total file
+
+DIR="/home/projects/vaccine/people/yatwan/cedar/"
+OUTDIR="${DIR}output/"
+DATADIR="${DIR}${1}"
+SCRIPTDIR="${DIR}scripts/"
+
+for k in 8 9 10 11 15
+do
+  cd $SCRIPTDIR
+  # TMP dir for file splitting; TODO
+  # mkdir "${SCRIPTDIR}tmp/"
+
+  python generate_kmers.py -filepath $DATADIR -k $k -outdir $OUTDIR -description_verbose "false" -drop_sequence "true"
+  cd $OUTDIR
+  # concatenate the splits
+  cat *${k}mers*.txt > ${k}mers_${2}.txt
+  # Remove all the other split
+  rm *split*.txt
+  awk -F ',' 'NR>1 {print $1}' ${k}mers_${2}.txt  >> ${k}mers_${2}.pep
+  netMHCpan4-1 -BA -xls -a HLANAMES HERE -xlsfile ${k}mers_netmhcpan_out.xls -p ${k}mers_${2}.pep
+done
+
+
