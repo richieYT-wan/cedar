92665b0e847142a26cf4edd93e77ce2a90a35e54
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2022-05-23 20:49:31 +0200

    change script args

--- scripts/kmers_pipeline.sh
+++ scripts/kmers_pipeline.sh
@@ -12,7 +12,7 @@ source activate phd
 
 DIR="/home/projects/vaccine/people/yatwan/cedar/"
 OUTDIR="${DIR}output/"
-DATADIR="${DIR}${1}"
+DATADIR="${DIR}data/human_split/"
 SCRIPTDIR="${DIR}scripts/"
 for k in 8 9 10 11 12 15
 do
@@ -24,16 +24,16 @@ do
   python generate_kmers.py -filepath $DATADIR -k $k -outdir $OUTDIR -description_verbose "false" -drop_sequence "true"
   cd $OUTDIR
   # concatenate the splits
-  cat *${k}mers*.txt > ${k}mers_${2}.txt
+  cat *${k}mers*.txt > ${k}mers_human_proteome.txt
   # Remove all the other split
   rm *split*.txt
   # Dedupe here + saving merged to txt and pep
-  python "${SCRIPTDIR}remove_duplicates.py" -filepath ${OUTDIR}${k}mers_${2}.txt
+  python "${SCRIPTDIR}remove_duplicates.py" -filepath ${OUTDIR}${k}mers_human_proteome.txt
 
-  netMHCpan-4.1 -BA -xls -a HLA-A02:01,HLA-B07:02,HLA-A03:01,HLA-A24:02,HLA-A11:01,HLA-B15:01,HLA-B35:01,HLA-A02:06,HLA-B27:05,HLA-A01:01 -xlsfile ${OUTDIR}${k}mers_netmhcpan_out.xls -t 0.5 -p ${OUTDIR}${k}mers_${2}.pep
+  netMHCpan-4.1 -BA -xls -a HLA-A02:01,HLA-B07:02,HLA-A03:01,HLA-A24:02,HLA-A11:01,HLA-B15:01,HLA-B35:01,HLA-A02:06,HLA-B27:05,HLA-A01:01 -xlsfile ${OUTDIR}${k}mers_netmhcpan_out.xls -t 0.5 -p ${OUTDIR}${k}mers_human_proteome.pep
 
   cd $SCRIPTDIR
-  python score_kmers.py -filepath ${OUTDIR}${k}mers_${2}.pep -resultspath ${OUTDIR}${k}mers_netmhcpan_out.xls -rank_thr ${3}
+  python score_kmers.py -filepath ${OUTDIR}${k}mers_human_proteome.pep -resultspath ${OUTDIR}${k}mers_netmhcpan_out.xls -rank_thr 2.0
 done
 
 
