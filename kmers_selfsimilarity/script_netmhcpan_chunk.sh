24151b680422f83e8d88cd1f6ea8cac382110a79
richieYT-wan <riwa@dtu.dk>
2022-05-27 15:08:34 +0200

    changed netmhcpan script

--- kmers_selfsimilarity/script_netmhcpan_chunk.sh
+++ kmers_selfsimilarity/script_netmhcpan_chunk.sh
@@ -24,14 +24,14 @@ do
     for hla in 'HLA-A02:01' 'HLA-B07:02' 'HLA-A03:01' 'HLA-A24:02' 'HLA-A11:01' 'HLA-B15:01' 'HLA-B35:01' 'HLA-A02:06' 'HLA-B27:05' 'HLA-A01:01'
     do
     	hlaname="${hla//:}"
-    	filename="${hlaname}_${1}mers_${3}_chunk_${2}"
+    	filename="${hlaname}_${k}mers_humanproteome_chunk_${chunk}"
     	pepname="${OUTDIR}${k}mers_humanproteome_chunk_${chunk}.pep"
     	touch "${TMPDIR}${filename}.sh"
     	echo "#\! /usr/bin/bash" >> "${TMPDIR}${filename}.sh"
-    	echo PBS -N ${filename}
+    	echo PBS -N ${filename} >> "${TMPDIR}${filename}.sh"
     	echo netMHCpan-4.1 -a ${hla} -p ${pepname} -t 2.0 -BA -xls -xlsfile "${OUTDIR}${filename}.xls" > "${TMPDIR}${filename}.sh"
     	echo "# EOF" >> "${TMPDIR}${filename}.sh"
-    	qsub -d "${DIR}" -W group_list=vaccine -A vaccine -l nodes=1:ppn=1:thinnode,mem=12gb,walltime=3:00:00 "${TMPDIR}${filename}.sh"
+#    	qsub -d "${DIR}" -W group_list=vaccine -A vaccine -l nodes=1:ppn=1:thinnode,mem=12gb,walltime=3:00:00 "${TMPDIR}${filename}.sh"
     done
   done
 done
