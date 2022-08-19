8e100c16d2494d95852f30cc86f1c893c0cb436e
richieYT-wan <riwa@dtu.dk>
2022-08-19 12:58:14 +0200

    modified tune sh for DIRs

--- tune.sh
+++ tune.sh
@@ -7,7 +7,7 @@ HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
 PYDIR=${HOMEDIR}pyscripts/
 DATADIR=../data/partitioned_traindata/
 OUTDIR=../output/tuning/
-ICSDIR=../output/
+ICSDIR=../data/ic_dicts/
 cd ${PYDIR}
 pwd
 python3 ./tune_aafreq_methods.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -debug False>'xd.txt'
