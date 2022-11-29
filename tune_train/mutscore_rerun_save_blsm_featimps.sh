ef3875177a5580e43f6d0ee57e114df1a182ffc9
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2022-11-29 16:33:49 +0100

    Update mutscore_rerun_save_blsm_featimps.sh

--- tune_train/mutscore_rerun_save_blsm_featimps.sh
+++ tune_train/mutscore_rerun_save_blsm_featimps.sh
@@ -1,23 +1,23 @@
-for trainset in "cedar" "prime" "merged"
-do
-  # Here do the normal script
-  filename="${trainset}_normal_rerun_save_blsm.sh"
-  touch ${filename}
-  echo "#! /usr/bin/bash" >> ${filename}
-  echo "source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh" >> ${filename}
-  echo "source activate phd" >> ${filename}
-
-  echo "HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/" >> ${filename}
-  echo "PYDIR=\${HOMEDIR}pyscripts/" >> ${filename}
-  echo "DATADIR=../data/mutant/" >> ${filename}
-  echo "OUTDIR=../output/221122_mutscore_prime_fixed/" >> ${filename}
-  echo "ICSDIR=../data/ic_dicts/" >> ${filename}
-  echo "TRAINSET=${trainset}" >> ${filename}
-  echo "cd \${PYDIR}" >> ${filename}
-  echo "pwd" >> ${filename}
-  echo "python3 ./mutscores_train_eval_redosave.py -datadir \${DATADIR} -outdir \${OUTDIR} -icsdir \${ICSDIR} -trainset \${TRAINSET} -ncores 39"  >> ${filename}
-  qsub -W group_list=vaccine -A vaccine -l nodes=1:ppn=40:thinnode,mem=160gb,walltime=36:00:00 ${filename}
-done
+#for trainset in "cedar" "prime" "merged"
+#do
+#  # Here do the normal script
+#  filename="${trainset}_normal_rerun_save_blsm.sh"
+#  touch ${filename}
+#  echo "#! /usr/bin/bash" >> ${filename}
+#  echo "source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh" >> ${filename}
+#  echo "source activate phd" >> ${filename}
+#
+#  echo "HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/" >> ${filename}
+#  echo "PYDIR=\${HOMEDIR}pyscripts/" >> ${filename}
+#  echo "DATADIR=../data/mutant/" >> ${filename}
+#  echo "OUTDIR=../output/221122_mutscore_prime_fixed/" >> ${filename}
+#  echo "ICSDIR=../data/ic_dicts/" >> ${filename}
+#  echo "TRAINSET=${trainset}" >> ${filename}
+#  echo "cd \${PYDIR}" >> ${filename}
+#  echo "pwd" >> ${filename}
+#  echo "python3 ./mutscores_train_eval_redosave.py -datadir \${DATADIR} -outdir \${OUTDIR} -icsdir \${ICSDIR} -trainset \${TRAINSET} -ncores 39"  >> ${filename}
+#  qsub -W group_list=vaccine -A vaccine -l nodes=1:ppn=40:thinnode,mem=160gb,walltime=36:00:00 ${filename}
+#done
 
 for trainset in "cedar" "prime" "merged"
 do
@@ -36,7 +36,7 @@ do
   echo "TRAINSET=${trainset}" >> ${filename}
   echo "cd \${PYDIR}" >> ${filename}
   echo "pwd" >> ${filename}
-  echo "python3 ./mutscores_train_eval_maskcysteine_redosave.py -datadir \${DATADIR} -outdir \${OUTDIR} -icsdir \${ICSDIR} -trainset \${TRAINSET} -ncores 39"  ${filename}
+  echo "python3 ./mutscores_train_eval_maskcysteine_redosave.py -datadir \${DATADIR} -outdir \${OUTDIR} -icsdir \${ICSDIR} -trainset \${TRAINSET} -ncores 39" >> ${filename}
   qsub -W group_list=vaccine -A vaccine -l nodes=1:ppn=40:thinnode,mem=160gb,walltime=36:00:00 ${filename}
 done
 
