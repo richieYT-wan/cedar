3acf859ed50362d6215d8781815f5a15b34bed87
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2022-12-05 10:45:37 +0100

    Update manual_tune_nn.py

--- pyscripts/manual_tune_nn.py
+++ pyscripts/manual_tune_nn.py
@@ -1,4 +1,5 @@
 import os, sys
+
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
@@ -23,29 +24,26 @@ train_dataset = pd.read_csv('../data/mutant/221028_cedar_related_newcore_fold.cs
 eval_dataset = pd.read_csv('../data/mutant/221119_prime_related_10fold.csv')
 ics_dict = ics_kl
 
+DIR_ = f'../output/nn_manual_tune/'
+mkdirs(DIR_)
+
 training_kwargs = dict(n_epochs=1000, early_stopping=True, patience=500, delta=1e-6,
-                       filename='../output/nn_test/checkpoint_test',
+                       filename=f'{DIR_}checkpoint_test',
                        verbosity=1)
 encoding_kwargs = dict(max_len=12, encoding='onehot', blosum_matrix=None, standardize=True, seq_col='Peptide',
                        hla_col='HLA', target_col='agg_label', rank_col='EL_rank_mut',
                        mut_col=['mutation_score', 'ratio_rank'],
                        adaptive=False, mask=True, add_rank=True, add_aaprop=False, remove_pep=False)
 
-
-
-DIR_ = f'../output/nn_manual_tune/'
-mkdirs(DIR_)
-
 criterion = nn.BCELoss()
 device = 'cpu'
 n_jobs = 1
 
 
-
 def parallel_wrapper(lr, nh, nl):
     model = FFNetPipeline(n_in=23, n_hidden=nh, n_layers=nl, dropout=0.25)
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=5e-3)
-
+    training_kwargs['filename'] = f'{DIR_}lr{lr}_nh{nh}_nl{nl}_chkpt'
     models_dict, train_metrics, test_metrics = nested_kcv_train_nn(train_dataset, model, optimizer, criterion, device,
                                                                    ics_dict, encoding_kwargs, training_kwargs, n_jobs)
 
@@ -61,11 +59,14 @@ def parallel_wrapper(lr, nh, nl):
     print('test', np.mean([x['auc'] for _, x in test_metrics.items()]))
     print('prime concat', test_results['concatenated']['auc'])
 
-    results=[lr, nh, nl, np.mean([x[-1] for x in [v2['train']['auc'] for k1, v1 in train_metrics.items() for _, v2 in v1.items()]]),
-             np.mean([x[-1] for x in [v2['valid']['auc'] for k1, v1 in train_metrics.items() for _, v2 in v1.items()]]),
-             np.mean([x['auc'] for _, x in test_metrics.items()]),
-             test_results['concatenated']['auc']]
+    results = [lr, nh, nl, np.mean(
+        [x[-1] for x in [v2['train']['auc'] for k1, v1 in train_metrics.items() for _, v2 in v1.items()]]),
+               np.mean(
+                   [x[-1] for x in [v2['valid']['auc'] for k1, v1 in train_metrics.items() for _, v2 in v1.items()]]),
+               np.mean([x['auc'] for _, x in test_metrics.items()]),
+               test_results['concatenated']['auc']]
     return results
+
 
 lrs = [1e-5, 5e-5, 1e-4]
 nhs = [10, 20, 30, 40]
@@ -74,5 +75,6 @@ conditions = product(lrs, product(nhs, product(nls)))
 conditions = list(list(flatten_product(x)) for x in conditions)
 
 output = Parallel(n_jobs=4)(delayed(parallel_wrapper)(lr=lr, nh=nh, nl=nl) for lr, nh, nl in conditions)
-results = pd.DataFrame(output, columns = ['lr', 'n_hidden', 'n_layers', 'train_auc', 'valid_auc', 'test_auc', 'external_prime_auc'])
+results = pd.DataFrame(output, columns=['lr', 'n_hidden', 'n_layers', 'train_auc', 'valid_auc', 'test_auc',
+                                        'external_prime_auc'])
 results.to_csv(f'{DIR_}manual_tune_results.csv', index=False)
