0544097bac4b2b34ab983f1543e8390b4380b2c5
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2022-12-17 11:41:30 +0100

    Create non_top_hla_model.sh

---
+++ tune_train/non_top_hla_model.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh                                                      
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+PYDIR=${HOMEDIR}pyscripts/
+DATADIR=../data/mutant/
+OUTDIR=../output/221212_HLA_specific_model/
+ICSDIR=../data/ic_dicts/
+cd ${PYDIR}
+pwd
+python3 ./mutscores_train_eval_nontophla.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -ncores 39  > 'nontop_HLA_models.txt'
+
