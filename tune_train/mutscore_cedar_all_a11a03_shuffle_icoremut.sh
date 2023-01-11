fdf04befdfdaec13910925a4d2a6b1114339df90
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-01-11 14:50:19 +0100

    added script for no a11-a03

---
+++ tune_train/mutscore_cedar_all_a11a03_shuffle_icoremut.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh                                                      
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+PYDIR=${HOMEDIR}pyscripts/
+DATADIR=../data/mutant/
+OUTDIR=../output/230109_all_a11posnegshuffle/
+ICSDIR=../data/ic_dicts/
+cd ${PYDIR}
+pwd
+python3 ./mutscores_train_eval_all_a11a03_posnegshuffle.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -input_type icore_mut -ncores 39  > 'cedar_noa11a03_icoremut.txt'
+
--- tune_train/mutscore_cedar_all_a11posnegshuffle.sh
+++ tune_train/mutscore_cedar_all_a11posnegshuffle.sh
@@ -10,5 +10,5 @@ OUTDIR=../output/230109_all_a11posnegshuffle/
 ICSDIR=../data/ic_dicts/
 cd ${PYDIR}
 pwd
-python3 ./mutscores_train_eval_all_a11posnegshuffle.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -ncores 39  > 'cedar_noa11.txt'
+python3 ./mutscores_train_eval_all_a11posnegshuffle.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -ncores 39  > 'cedar_noa11a03.txt'
 
