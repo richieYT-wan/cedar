3a57a17135509a027107783a4f03afd85f72b885
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-01-12 16:36:38 +0100

    added no a03

---
+++ tune_train/mutscore_cedar_all_a03posnegshuffle.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh                                                      
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+PYDIR=${HOMEDIR}pyscripts/
+DATADIR=../data/mutant/
+OUTDIR=../output/230109_all_a03posnegshuffle/
+ICSDIR=../data/ic_dicts/
+cd ${PYDIR}
+pwd
+python3 ./mutscores_train_eval_all_a03posnegshuffle.py -datadir ${DATADIR} -outdir ${OUTDIR} -icsdir ${ICSDIR} -ncores 39  > 'cedar_noa11a03.txt'
+
