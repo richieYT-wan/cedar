240282224f3d76b22b7cfe2a94a465e56a8c5553
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-03-27 14:51:20 +0200

    added filtered.py

--- tune_train/aligned_icore_exprscore_mutscore_wtrank_foreign_onehot.sh
+++ tune_train/aligned_icore_exprscore_mutscore_wtrank_foreign_onehot.sh
@@ -5,10 +5,10 @@ source activate phd
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
 PYDIR=${HOMEDIR}pyscripts/
-OUTDIR=${HOMEDIR}output/230308_aligned_exprscore_addwtrank_foreignness/
+OUTDIR=${HOMEDIR}output/230327_aligned_icore_exprscore_mutscore_filtered/
 ICSDIR=../data/ic_dicts/
 TRAINSET="cedar"
 cd ${PYDIR}
 pwd
-python3 ./aligned_icore_exprscore_mutscore_train_eval_onehot.py -icsdir ${ICSDIR} -trainset ${TRAINSET} -add_wtrank True -add_foreignness True -ncores 39 -outdir ${OUTDIR}
+python3 ./aligned_icore_exprscore_mutscore_train_eval_onehot_filtered.py -icsdir ${ICSDIR} -trainset ${TRAINSET} -add_wtrank True -add_foreignness True -ncores 39 -outdir ${OUTDIR}
 
