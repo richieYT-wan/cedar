4b4e65707105920f22a7aa7450dedcbc645affb0
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-03-27 13:30:35 +0200

    added onehot only script to speed up..

---
+++ tune_train/aligned_icore_exprscore_mutscore_wtrank_foreign_onehot.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+PYDIR=${HOMEDIR}pyscripts/
+OUTDIR=${HOMEDIR}output/230308_aligned_exprscore_addwtrank_foreignness/
+ICSDIR=../data/ic_dicts/
+TRAINSET="cedar"
+cd ${PYDIR}
+pwd
+python3 ./aligned_icore_exprscore_mutscore_train_eval_onehot.py -icsdir ${ICSDIR} -trainset ${TRAINSET} -add_wtrank True -add_foreignness True -ncores 39 -outdir ${OUTDIR}
+
