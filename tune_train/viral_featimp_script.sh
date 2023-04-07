90e8d897c5fbecfc70edafc335c95a0a41739f08
richieYT-wan <riwa@dtu.dk>
2023-04-07 14:43:46 +0200

    updated viral script (parallelized)

--- tune_train/viral_featimp_script.sh
+++ tune_train/viral_featimp_script.sh
@@ -4,10 +4,10 @@ source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
 source activate phd
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
-OUTDIR=${HOMEDIR}output/viral_featimp_sampling/
+OUTDIR=${HOMEDIR}output/230401_viral_featimp_sampling/
 PYDIR=${HOMEDIR}pyscripts/
 ICSDIR=../data/ic_dicts/
 cd ${PYDIR}
 pwd
-python3 ./virus_feature_importances.py -icsdir ${ICSDIR} -ncores 39  > 'tmp.txt'
+python3 ./virus_feature_importances.py -icsdir ${ICSDIR} -outdir ${OUTDIR} -ncores 39  > 'tmp.txt'
 
