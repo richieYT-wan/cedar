5b1734e3619ceef61ee30d01a354b085349b02f9
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-04-11 09:32:12 +0200

    ....... didn't save.....

--- tune_train/best_models_addexpr_redo.sh
+++ tune_train/best_models_addexpr_redo.sh
@@ -5,10 +5,10 @@ source activate phd
 
 HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
 PYDIR=${HOMEDIR}pyscripts/
-OUTDIR=${HOMEDIR}output/230405_ExpandEnsembleResample/
+OUTDIR=${HOMEDIR}output/230410_redo_AddExpression_BestModels/
 ICSDIR=../data/ic_dicts/
 TRAINSET="cedar"
 cd ${PYDIR}
 pwd
-python3 ./expand_ensemble_resample.py -icsdir ${ICSDIR} -trainset ${TRAINSET} -ncores 39 -outdir ${OUTDIR}
+python3 ./best_models_addexpr.py -icsdir ${ICSDIR} -trainset ${TRAINSET} -ncores 39 -outdir ${OUTDIR}
 
