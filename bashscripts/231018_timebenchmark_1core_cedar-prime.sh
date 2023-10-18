22b520d5bc67c2b6854f1cb89da48d43171973ce
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-10-18 15:31:32 +0200

    This should work now

---
+++ bashscripts/231018_timebenchmark_1core_cedar-prime.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+PYDIR=${HOMEDIR}pyscripts/
+OUTDIR=${HOMEDIR}output/231018_redo_merged/
+ICSDIR=${HOMEDIR}/data/ic_dicts/
+TRAINSET="cp_merged"
+CONDITION="KL-Mask"
+INDEX="1023"
+INPUTTYPE="icore_mut"
+cd ${PYDIR}
+pwd
+echo "Starting PyScript"
+python3 ./231018_mutexpr_merged_trainsets.py -icsdir ${ICSDIR} -ncores 1 -outdir ${OUTDIR} -trainset cp_merged -condition KL-Mask -input_type icore_mut -mc_index 1023
+
