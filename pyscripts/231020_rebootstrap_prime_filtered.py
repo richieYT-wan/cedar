9a00c538b7b8bdad6eef25243b751f38f83febc5
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-10-20 15:55:25 +0200

    please work

--- pyscripts/231020_rebootstrap_prime_filtered.py
+++ pyscripts/231020_rebootstrap_prime_filtered.py
@@ -21,16 +21,32 @@ from src.bootstrap import get_pval_wrapper, bootstrap_eval
 
 N_CORES = 39
 
-def get_in_ref(peptide, hla, ref):
-    return len(ref.query('Peptide==@peptide and HLA == @hla'))==1
 
-def pipeline(df, baseline, ref):
+def get_cdt(basename):
+    basename = basename.split('onehot_')[1].split('.csv')[0]
+    weight, key = basename.split('_icore_mut_')
+    return weight, key
+
+
+def get_in_ref(peptide, hla, ref):
+    return len(ref.query('Peptide==@peptide and HLA == @hla')) == 1
+
+
+def bootstrap_pipeline(filename, args, ref):
+    df = pd.read_csv(filename)
+    basename = os.path.basename(filename)
+    weight, key = get_cdt(basename)
+
     df['in_nepdb'] = df.apply(lambda x: get_in_ref(x['Peptide'], x['HLA'], ref=ref), axis=1)
     df = df.query('not in_nepdb')
     pcol = 'pred' if 'pred' in df.columns else 'mean_pred'
-    df = bootstrap_eval(df[pcol].values, df['agg_label'].values, 10000, 10, True, False, True)
+    bdf = bootstrap_eval(df[pcol].values, df['agg_label'].values, 10000, 10, True, False, True)\
+                        .assign(evalset='PRIME', input_type='icore_mut', weight=weight, key=key)
+    bdf.to_csv(f'{args["outdir"]}PRIME_bootstrapped_df_cedar_onehot_{weight}_icore_mut_{key}.csv')
+    return None
 
-    evalset = 'PRIME'
+def compare_baseline(df, baseline):
+    evalset = df.evalset.unique()[0]
     b = baseline.query('evalset==@evalset')
     baseline_icore = b.query('input_type=="icore_mut"')
     baseline_pep = b.query('input_type=="Peptide"')
@@ -47,15 +63,15 @@ def args_parser():
     parser = argparse.ArgumentParser(
         description='Script to crossvalidate and evaluate methods that use aa frequency as encoding')
 
-    parser.add_argument('-datadir', type=str, default='/home/projects/vaccine/people/yatwan/cedar/output/230427_MutExpr_Final_input_type/prime_filtered/',
+    parser.add_argument('-datadir', type=str,
+                        default='/home/projects/vaccine/people/yatwan/cedar/output/230427_MutExpr_Final_input_type/prime_filtered/',
                         help='Path to directory containing the results')
-    parser.add_argument('-savename', type=str, default='')
+    parser.add_argument('-savename', type=str, default='recompare_harmonic')
     parser.add_argument('-outdir', type=str,
-                        default='../output/230427_MutExpr_Final_input_type/')
+                        default='/home/projects/vaccine/people/yatwan/cedar/output/230427_MutExpr_Final_input_type/cp_filtered_outdir/')
     parser.add_argument('-ncores', type=int, default=40,
                         help='N cores to use in parallel, by default will be multiprocesing.cpu_count() * 3/4')
     return parser.parse_args()
-
 
 
 def main():
@@ -69,15 +85,29 @@ def main():
     baselines = os.path.join(args['datadir'], 'baselines/')
     baseline_df = pd.concat([pd.read_csv(f'{baselines}{x}') for x in os.listdir(baselines)])
     nepdb = pd.read_csv(f'{args["datadir"]}NEPDB_preds_cedar_onehot_None_icore_mut_only_rank.csv')
-    files = [x for x in os.listdir(args["datadir"]) if x.endswith('.csv') and 'PRIME' in x]
+    files = [f'{args["datadir"]}{x}' for x in os.listdir(args["datadir"]) if x.endswith('.csv') and 'PRIME' in x]
     # This will be a list of lists initially, then concat along axis = 1 for each evalset
-    res_list = []
-    baseline_wrapper = partial(pipeline, baseline=baseline_df, ref=nepdb)
+    print('Doing BDFs')
+    bs_wrapper = partial(bootstrap_pipeline, args=args, ref=nepdb)
     output = Parallel(n_jobs=4)(
-        delayed(baseline_wrapper)(df=pd.read_csv(f'{args["datadir"]}{x}')) for x in tqdm(files, desc='files', position=1, leave=False))
-    res_list.append(pd.concat(output, axis=0))
+        delayed(bs_wrapper)(filename=x) for x in
+        tqdm(files, desc='files', position=1, leave=False))
+
+    print('Doing baseline comparisons')
+    baseline_wrapper = partial(compare_baseline, baseline=baseline_df)
+    bdf_files = [x for x in os.listdir(args["outdir"]) if x.endswith('.csv')]
+    res_list = []
+    for evalset in tqdm(['CEDAR','PRIME'], desc='evalset', position=0, leave=True):
+        filtered_files = list(filter(lambda x: evalset in x and x.startswith(evalset), bdf_files))
+        if len(list(filtered_files)) == 0: continue
+        output = Parallel(n_jobs=args['ncores'])(
+            delayed(baseline_wrapper)(df=pd.read_csv(f'{args["outdir"]}{x}')) for x in
+            tqdm(filtered_files, desc='files', position=1, leave=False))
+        res_list.append(pd.concat(output, axis=0))
 
     pd.concat(res_list, axis=1).to_csv(f'{args["outdir"]}{args["savename"]}_gb_results.csv')
+
+
     end = dt.now()
     elapsed = divmod((end - start).seconds, 60)
     print(f'Elapsed: {elapsed[0]} minutes, {elapsed[1]} seconds. ; Memory used: {tracemalloc.get_traced_memory()}')
