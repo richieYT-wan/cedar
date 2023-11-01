aa2540b976dd7b9f309348ddebd68b3f11130f84
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2023-11-01 15:44:20 +0100

    Create 231101_redo_viral.sh

---
+++ bashscripts/231101_redo_viral.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+source /home/projects/vaccine/people/yatwan/anaconda3/etc/profile.d/conda.sh
+source activate phd
+
+HOMEDIR=/home/projects/vaccine/people/yatwan/cedar/
+OUTDIR=${HOMEDIR}output/230401_viral_featimp_sampling_interative/
+PYDIR=${HOMEDIR}pyscripts/
+ICSDIR=../data/ic_dicts/
+cd ${PYDIR}
+pwd
+python3 ./231101_redo_adding_virus_merged.py -icsdir ${ICSDIR} -outdir ${OUTDIR} -ncores 40  > 'virout.txt'
+
